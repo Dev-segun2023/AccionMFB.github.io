c6d6da39e2aed830afad3ec0a83cea456b2defbf
Dev-segun2023 <segundavidjohn@gmail.com>
2025-02-13 14:33:49 +0100

    every feature working well, but need improvement

---
+++ script.js
@@ -0,0 +1,22 @@
+let menuBar = document.querySelector('.bars');
+let aside = document.querySelector('.aside');
+menuBar.addEventListener('click', () => {
+  // Toggle the 'active' class to apply or remove the transition
+  aside.classList.toggle('active');
+});
+
+let cancel = document.querySelector('.close');
+let popover = document.querySelector('.popover');
+
+setTimeout(() => {
+  popover.style.display = 'block';
+ }, 5000);
+
+ window.onclick = () => {
+  popover.style.display = 'none';
+}
+
+
+cancel.addEventListener('click', () => {
+  popover.style.display = 'none';
+})
